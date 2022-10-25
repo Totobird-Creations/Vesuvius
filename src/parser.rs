a350055ea406429192c758f264a16c123ac826d7
Totobird Creations <45902300+Totobird-Creations@users.noreply.github.com>
2022-10-25 15:51:25 +0000

    Savepoint;

--- src/parser.rs
+++ src/parser.rs
@@ -5,23 +5,45 @@ use peg;
 
 pub struct Declaration {
     headers : Vec<HeaderType>,
-
+}
+impl Declaration {
+    pub fn from(headers : Vec<HeaderType>) -> Declaration {
+        return Declaration {
+            headers : headers
+        };
+    }
+    pub fn to_header(headers : Vec<&str>) -> Vec<HeaderType> {
+        return headers.iter().map(|h| match *h {
+            "entry" => HeaderType::Entry,
+            "pub"   => HeaderType::Public,
+            _       => panic!("INTERNAL ERROR")
+        }).collect::<Vec<HeaderType>>();
+    }
 }
 pub enum HeaderType {
-    Entry
+    Entry,
+    Public
 }
 
 
 pub fn read(path : &str) -> Vec<Declaration> {
-    return parser::program(fs::read_to_string(path)).unwrap();
+    return parser::program(&fs::read_to_string(path).unwrap()).unwrap();
 }
 
 
 peg::parser! {
-    grammar parser() for String {
-        use super::Nodes;
+    grammar parser() for str {
 
-        pub program() -> Vec<Nodes>
-            = e:(declaration ";")* {e}
+        pub rule program() -> Vec<Declaration>
+            = e:(declaration() ** ";")
+            {e}
+
+        rule declaration() -> Declaration
+            = h:declaration_header()
+            {Declaration::from(h)}
+
+        rule declaration_header() -> Vec<HeaderType>
+            = h:$("#[" ("entry" / "pub") "]")*
+            {Declaration::to_header(h)}
     }
 }
